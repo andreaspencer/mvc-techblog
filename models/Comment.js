4dac87f7941743cd72064164871793139bde0961
andreaspencer <andreaaddawn@gmail.com>
2021-11-14 15:13:00 -0800

    model for comment made

---
+++ models/Comment.js
@@ -0,0 +1,43 @@
+const { Model, dataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGAR,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        comment_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGAR,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGAR,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        modelName: 'comment'
+    }
+)
+
+module.exports = Comment;
