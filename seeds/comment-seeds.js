d82a71bb91f605fde536b8fd677d257b7086b9ef
andreaspencer <andreaaddawn@gmail.com>
2021-11-14 16:03:02 -0800

    seed comments

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,38 @@
+const { Comment } = require('../models');
+
+const commentData = [
+    {
+        comment_text: "",
+        post_id: 1,
+        user_id: 3
+    },
+    {
+        comment_text: "",
+        post_id: 2,
+        user_id: 2
+    },
+    {
+        comment_text: "",
+        post_id: 3,
+        user_id: 4
+    },
+    {
+        comment_text: "",
+        post_id: 4,
+        user_id: 1
+    },
+    {
+        comment_text: "",
+        post_id: 5,
+        user_id: 3
+    },
+    {
+        comment_text: "",
+        post_id: 5,
+        user_id: 4
+    },
+];
+
+const seedComments = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComments;
